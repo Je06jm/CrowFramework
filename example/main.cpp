e4594055cfe3d1c7c394f2717f545dbd4ab04ed7
Je06jm <jeremymoyes3@gmail.com>
2024-04-07 19:50:58 -0600

    Updated example

--- example/main.cpp
+++ example/main.cpp
@@ -1,67 +1,81 @@
 #include <iostream>
 
 #include <crow/Actor.hpp>
+#include <crow/Application.hpp>
+#include <crow/Logging.hpp>
 #include <crow/Window.hpp>
 
-#include <thread>
-
-struct AMessage : public crow::MessageBase {
-    int num;
-};
-
-struct BMessage : public crow::MessageBase {
-    int data;
-};
-
-class AActor : public crow::ActorBase<AMessage> {
+class IntActor : public crow::Actor<int> {
 public:
-    void HandleMessage(std::unique_ptr<AMessage> msg) override {
-        std::cout << "AActor: " << msg->num << std::endl;
+    void HandleMessage(std::unique_ptr<int>&& msg) override {
+        crow::app::Info("IntActor: {}", *msg);
 
-        BMessage bmsg;
-        bmsg.data = msg->num + 10;
-
-        auto b_scheduler = crow::ActorScheduler::GetScheduler(
-            crow::ATTRIBUTE_ACTOR_SCHEDULER_RENDERING);
-
-        b_scheduler->SendMessage(std::move(bmsg));
+        crow::actor_scheduler->SendMessage<float>(std::make_unique<float>(*msg));
     }
 };
 
-class BActor : public crow::ActorBase<BMessage> {
+class FloatActor : public crow::Actor<float> {
 public:
-    void HandleMessage(std::unique_ptr<BMessage> msg) override {
-        std::cout << "BActor: " << msg->data << std::endl;
+    void HandleMessage(std::unique_ptr<float>&& msg) override {
+        crow::app::Warning("FloatActor: {}", *msg);
     }
 };
+
+class ExampleApplication : public crow::Application {
+protected:
+    size_t GetThreadCount() const override {
+        return 4; // Only use 4 threads (Including main)
+        //return 1; // Only use main thread
+        //return crow::Application::GetThreadCount(); // Use all the cores!
+    }
+
+    void OnPreActorSchedulerSetup() override {
+        // Setup before actor_manager is init
+        crow::SetLoggingFile("log.txt");
+    }
+
+    void OnRegisterActors() override {
+        crow::actor_scheduler->Register<IntActor>();
+        crow::actor_scheduler->Register<FloatActor>();
+    }
+
+    void OnPostActorSchedulerSetup() override {
+        // Setup after actor_manager is init
+        crow::actor_scheduler->EmplaceMessageAs<crow::WindowMessageBase>(crow::WindowSetResolution({200, 200}));
+        crow::actor_scheduler->EmplaceMessageAs<crow::WindowMessageBase>(crow::WindowCreate{});
+        
+        crow::actor_scheduler->EmplaceMessage<float>(3.14);
+        crow::actor_scheduler->EmplaceMessage<int>(1);
+
+        crow::actor_scheduler->SendMessage(std::make_unique<int>(69));
+    }
+
+    void OnUpdate() override {
+        crow::actor_scheduler->EmplaceMessageAs<crow::WindowMessageBase>(crow::WindowShouldClose(
+            [&](bool close) {
+                if (close) {
+                    Exit();
+                }
+            }
+        ));
+
+        crow::actor_scheduler->EmplaceMessageAs<crow::WindowMessageBase>(crow::WindowUpdate{});
+    }
+
+    void OnPreActorSchedulerCleanup() override {
+        // Cleanup before actor_manager is cleanup
+        crow::actor_scheduler->EmplaceMessage<int>(42);
+    }
+
+    void OnPostActorSchedulerCleanup() override {
+        // Cleanup after actor_manager is cleanup
+    }
+};
+
+//CROW_APPLICATION(ExampleApplication)
 
 int main() {
-    auto a_scheduler = crow::ActorScheduler::CreateNewScheduler();
-
-    a_scheduler->Spawn<AActor>();
-
-    auto a_processor = std::jthread([&]() { a_scheduler->Run(); });
-
-    auto b_scheduler = crow::ActorScheduler::CreateNewScheduler(
-        crow::ATTRIBUTE_ACTOR_SCHEDULER_RENDERING);
-
-    b_scheduler->Spawn<BActor>();
-
-    auto b_processor = std::jthread([&]() { b_scheduler->Run(); });
-
-    auto window = crow::Window::CreateWindow();
-    window->Create();
-
-    AMessage msg;
-    msg.num = 1;
-
-    a_scheduler->SendMessage(std::move(msg));
-
-    while (!window->ShouldClose()) { window->Update(); }
-
-    a_scheduler->BlockUntilEmpty();
-    b_scheduler->BlockUntilEmpty();
-
-    a_scheduler->Stop();
-    b_scheduler->Stop();
+    ExampleApplication app;
+    app._InternalRun();
+    return 0;
 }
